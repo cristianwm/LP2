d12520540879aa86b4979e73f7822f0c02b442b3
cristianwm <88410444+cristianwm@users.noreply.github.com>
2021-11-19 21:34:28 -0300

    Add files via upload

---
+++ prova/PROVA/P0030482113020/P0030482113020/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace P0030482113020
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lstbxDados = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(214, 131);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(142, 95);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstbxDados
+            // 
+            this.lstbxDados.FormattingEnabled = true;
+            this.lstbxDados.ItemHeight = 20;
+            this.lstbxDados.Location = new System.Drawing.Point(499, 21);
+            this.lstbxDados.Name = "lstbxDados";
+            this.lstbxDados.Size = new System.Drawing.Size(262, 404);
+            this.lstbxDados.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstbxDados);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lstbxDados;
+    }
+}
+
---
+++ prova/PROVA/P0030482113020/P0030482113020/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace P0030482113020
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                double[] Semana = new double[4];
+                double TotalMes = 0, Total = 0, seman = 0;
+                int i, j;
+                string auxiliar = "";
+                double gambi = 0;
+
+
+
+                for (i = 0; i < 10; i++)
+                {
+                    for (j = 0; j < 4; j++)
+                    {
+                        auxiliar = Interaction.InputBox("Total vendido na " +
+                         (j + 1) + "ª semana".ToString(), (i + 1) + "º mês");
+
+                        if (!double.TryParse(auxiliar, out Semana[j]))
+
+                        {
+                            MessageBox.Show("Numero invalido,digite novamente!");
+                            j--;
+
+                        }
+                        else
+                        {
+                            seman = Semana[j];
+                            TotalMes += Semana[j];
+                            gambi = TotalMes;
+
+
+
+                        }
+
+                        Total += seman;
+                        lstbxDados.Items.Insert(0, "Total do mês " + (i + 1).ToString() + " Semana " + (j + 1).ToString() + " " + seman.ToString("C2"));
+
+
+                    }
+
+
+                    lstbxDados.Items.Insert(0, " " + "Total do mês " + " " + TotalMes.ToString("C2"));
+                    lstbxDados.Items.Insert(0, "------------------------------------------------------");
+                    TotalMes -= gambi;
+
+                }
+
+
+
+                lstbxDados.Items.Insert(0, ">> " + "Total geral " + " " + Total.ToString("C2"));
+                lstbxDados.Items.Insert(0, "------------------------------------------------------");
+            }
+        }
+    }
+}
